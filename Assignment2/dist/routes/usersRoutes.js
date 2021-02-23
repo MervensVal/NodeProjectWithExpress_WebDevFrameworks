0ba9e0cdc5f2f8f0ad69b9d2e6fa6138a8369c5f
Mervens Val <valmervens@gmail.com>
2021-02-23 17:50:34 -0500

    patch request added JSON

--- Assignment2/dist/routes/usersRoutes.js
+++ Assignment2/dist/routes/usersRoutes.js
@@ -10,7 +10,7 @@ const userRouter = express_1.default.Router();
 exports.userRouter = userRouter;
 //creating an array of type User
 let usersArray = [];
-usersArray.push(new user_1.User("1", "merv", "val", "merv@email.com"));
+//usersArray.push(new User("1","merv","val","merv@email.com"))
 //Get request
 userRouter.get('/', (req, res, next) => {
     res.status(200).send(usersArray);
@@ -19,5 +19,25 @@ userRouter.get('/', (req, res, next) => {
 userRouter.post('/', (req, res, next) => {
     usersArray.push(new user_1.User(req.body.userId, req.body.firstName, req.body.lastName, req.body.emailAddress));
     res.status(201).send(usersArray[usersArray.length - 1]);
+});
+//Patch Request
+userRouter.patch('/', (req, res, next) => {
+    let Id = req.body.userId;
+    let foundUser = null;
+    for (let i = 0; i < usersArray.length; i++) {
+        if (usersArray[i].userId === Id) {
+            foundUser = usersArray[i];
+            foundUser.firstName = req.body.firstName;
+            foundUser.lastName = req.body.lastName;
+            foundUser.emailAddress = req.body.emailAddress;
+            break;
+        }
+    }
+    if (foundUser === null) {
+        res.status(404).send({ message: 'User was not found' });
+    }
+    else {
+        res.send(foundUser);
+    }
 });
 //# sourceMappingURL=usersRoutes.js.map
--- Assignment2/src/routes/usersRoutes.ts
+++ Assignment2/src/routes/usersRoutes.ts
@@ -5,7 +5,7 @@ const userRouter = express.Router();
 //creating an array of type User
 let usersArray:User[] = [];
 
-usersArray.push(new User("1","merv","val","merv@email.com"))
+//usersArray.push(new User("1","merv","val","merv@email.com"))
 
 
 //Get request
@@ -17,6 +17,29 @@ userRouter.get('/',(req,res,next)=>{
 userRouter.post('/',(req,res,next)=>{
     usersArray.push(new User(req.body.userId,req.body.firstName,req.body.lastName,req.body.emailAddress));
     res.status(201).send(usersArray[usersArray.length - 1]);
+});
+
+//Patch Request
+userRouter.patch('/',(req,res,next)=>{
+    let Id = req.body.userId;
+    let foundUser: User|null = null;
+
+    for (let i = 0; i < usersArray.length; i++){
+        if (usersArray[i].userId === Id){
+            foundUser = usersArray[i];
+            foundUser.firstName = req.body.firstName;
+            foundUser.lastName = req.body.lastName;
+            foundUser.emailAddress = req.body.emailAddress;
+            break;
+        }
+    }
+    if(foundUser===null){
+        res.status(404).send({message:'User was not found'})
+    }
+    else {
+        res.send(foundUser);
+    }
+
 });
 
 //This router is exported and used in userApp
